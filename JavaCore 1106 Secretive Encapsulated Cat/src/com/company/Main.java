b010d5b1ce264e474b5ea53f4536fb3942eb4fc3
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-28 09:04:56 +0200

    Add files via upload

---
+++ JavaCore 1106 Secretive Encapsulated Cat/src/com/company/Main.java
@@ -0,0 +1,97 @@
+package com.company;
+
+/*
+
+1106 Secretive Encapsulated Cat
+Hide all Cat class internal variables.
+Requirements:
+1. The Cat variable name must be hidden.
+2. The age variable of the Cat class must be hidden.
+3. The weight variable of the Cat class must be hidden.
+4. There must be 3 variables in the Cat class.
+5. All Cat class variables must be private.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args)
+    {
+    }
+
+    public class Worker
+    {
+        private String name;
+    }
+
+    public class Clerk extends Worker
+    {
+
+    }
+
+    public class IT extends Worker
+    {
+
+    }
+
+    public class Programmer extends IT
+    {
+
+    }
+
+    public class ProjectManager extends IT
+    {
+
+    }
+
+    public class CTO extends IT
+    {
+
+    }
+
+    public class OfficeManager extends Clerk
+    {
+
+    }
+
+    public class HR extends Clerk
+    {
+
+    }
+
+    public class Cleaner extends Clerk
+    {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
